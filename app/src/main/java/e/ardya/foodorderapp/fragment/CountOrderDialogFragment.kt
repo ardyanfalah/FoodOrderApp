be18eb58292a12a359b047f26494facd3c18e66e
Ardyan Falah <ardyan.falah@sangkuriang.co.id>
2021-01-12 18:15:27 +0700

    fragment layout

---
+++ app/src/main/java/e/ardya/foodorderapp/fragment/CountOrderDialogFragment.kt
@@ -0,0 +1,18 @@
+package e.ardya.foodorderapp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class CountOrderDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
