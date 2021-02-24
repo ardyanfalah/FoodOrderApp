1ff4713b1eafa778475ef6b18ae0920e5dc33fd0
ardyanfalah <ardyanfalah@gmail.com>
2021-02-24 16:07:32 +0700

    tambah style beberapa

--- app/src/main/java/e/ardya/foodorderapp/adapter/RecycleMenuOrderAdapter.kt
+++ app/src/main/java/e/ardya/foodorderapp/adapter/RecycleMenuOrderAdapter.kt
@@ -6,33 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import e.ardya.foodorderapp.R
-import e.ardya.foodorderapp.data.model.MenuModel
 import e.ardya.foodorderapp.data.model.TransaksiModel
-import e.ardya.foodorderapp.data.net.RetrofitClient
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-class RecycleMenuOrderAdapter(private val context: Context, private val listOrder : ArrayList<TransaksiModel.ItemTransaksi>, private val listener : Listener) : RecyclerView.Adapter<RecycleMenuOrderAdapter.ViewHolder>() {
+class RecycleMenuOrderAdapter(
+    private val context: Context,
+    private val listOrder: ArrayList<TransaksiModel.ItemTransaksi>,
+    private val listener: Listener
+) : RecyclerView.Adapter<RecycleMenuOrderAdapter.ViewHolder>() {
     interface Listener {
-        fun onItemClick(order :TransaksiModel.ItemTransaksi)
-        fun onAddOrder(order :TransaksiModel.ItemTransaksi, position: Int)
-        fun onRemoveOrder(order :TransaksiModel.ItemTransaksi,position: Int)
+        fun onItemClick(order: TransaksiModel.ItemTransaksi)
+        fun onAddOrder(order: TransaksiModel.ItemTransaksi, position: Int)
+        fun onRemoveOrder(order: TransaksiModel.ItemTransaksi, position: Int)
     }
+
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         var itemNama: TextView = itemView.findViewById(R.id.tv_menu_name_item)
         var jumlahPesanan: TextView = itemView.findViewById(R.id.tv_menu_count)
         var itemHarga: TextView = itemView.findViewById(R.id.tv_menu_price_item)
+        var itemHargaTotal: TextView = itemView.findViewById(R.id.tv_harga_total_item)
         var btnAdd: Button = itemView.findViewById(R.id.btn_add_count)
         var btnRemove: Button = itemView.findViewById(R.id.btn_remove_count)
-        fun bind(order: TransaksiModel.ItemTransaksi, listener: Listener, position:Int){
-            itemView.setOnClickListener{ listener.onItemClick(order) }
-            btnAdd.setOnClickListener{ listener.onAddOrder(order,position) }
-            btnRemove.setOnClickListener{ listener.onRemoveOrder(order,position) }
+        fun bind(order: TransaksiModel.ItemTransaksi, listener: Listener, position: Int) {
+            itemView.setOnClickListener { listener.onItemClick(order) }
+            btnAdd.setOnClickListener { listener.onAddOrder(order, position) }
+            btnRemove.setOnClickListener { listener.onRemoveOrder(order, position) }
         }
 
         init {
@@ -40,6 +44,7 @@ class RecycleMenuOrderAdapter(private val context: Context, private val listOrde
             jumlahPesanan.text = "1"
         }
     }
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
         val v = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.item_order_detail, viewGroup, false)
@@ -48,17 +53,20 @@ class RecycleMenuOrderAdapter(private val context: Context, private val listOrde
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        val localeID = Locale("in", "ID")
+        val formatRupiah: NumberFormat = NumberFormat.getCurrencyInstance(localeID)
         var tempHarga = 0
-        tempHarga = if(listOrder[i].jumlah_pesan!=null && listOrder[i].harga_menu != null){
+        tempHarga = if (listOrder[i].jumlah_pesan != null && listOrder[i].harga_menu != null) {
             listOrder[i].jumlah_pesan!!.toInt().times(listOrder[i].harga_menu!!.toInt())
         } else {
             listOrder[i].harga_menu!!.toInt()
         }
-        Log.d("bind=>",i.toString())
-        viewHolder.bind(listOrder[i],listener,i)
+        Log.d("bind=>", i.toString())
+        viewHolder.bind(listOrder[i], listener, i)
         viewHolder.itemNama.text = listOrder[i].nama_menu
-        viewHolder.itemHarga.text = tempHarga.toString()
+        viewHolder.itemHarga.text = formatRupiah.format(listOrder[i].harga_menu?.toInt())
         viewHolder.jumlahPesanan.text = listOrder[i].jumlah_pesan.toString()
+        viewHolder.itemHargaTotal.text = formatRupiah.format(tempHarga)
     }
 
     override fun getItemCount(): Int {
