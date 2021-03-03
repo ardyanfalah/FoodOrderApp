1da68904d7a12e21ed3f7e523d258d074f2e1378
Ardyan Falah <ardyan.falah@sangkuriang.co.id>
2021-03-03 18:49:50 +0700

    tambah

--- app/src/main/java/e/ardya/foodorderapp/viewmodel/LoginViewModel.kt
+++ app/src/main/java/e/ardya/foodorderapp/viewmodel/LoginViewModel.kt
@@ -34,37 +34,9 @@ class LoginViewModel: BaseVM() {
         } else {
             dataLoading.postValue(false)
             toastMessage.postValue("Email dan Password tidak Boleh kosong")
+            
         }
     }
 
-//    fun clickLogin() {
-//        if (email.value != null && password.value != null) {
-//            dataLoading.postValue(true)
-//            UserService.login(email.value!!, password.value!!, {
-//                if (it?.user?.roleDataId == StaticValue.ROLE_FASKES || it?.user?.roleDataId == StaticValue.ROLE_DONATUR) {
-//                    SessionHelper.save("name", it.user?.name ?: "-")
-//                    SessionHelper.save("profilePicture", it.user?.profilePicture ?: "")
-//                    SessionHelper.save("username", username.value)
-//                    SessionHelper.save("password", password.value)
-//                    SessionHelper.save("token", "${it.tokenType} ${it.accessToken}")
-//                    SessionHelper.save("id", it.user?.id)
-//                    SessionHelper.save("email", it.user?.email)
-//                    SessionHelper.save("phone1", it.user?.mobilePhone)
-//                    SessionHelper.save("roleId", it.user?.roleDataId)
-//                    SessionHelper.save("roleName", it.user?.roleDataName)
-//                    getMe()
-//                    dataLoading.postValue(false)
-//                } else {
-//                    dataLoading.postValue(false)
-//                }
-//
-//            }, {
-//                dataLoading.postValue(false)
-//                toastMessage.postValue(it ?: "Silahkan coba lagi")
-//            })
-//        } else {
-//            toastMessage.postValue("Username & Password tidak boleh kosong.")
-//        }
-//    }
 
 }
